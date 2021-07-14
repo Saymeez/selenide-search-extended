1fdb00b8ad8fa9aebe0af42ec47242270c65c74a
Saymeez <52138103+Saymeez@users.noreply.github.com>
2021-07-14 18:49:30 +0300

    TestsForStudentRegistrationForm class

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -1,5 +1,14 @@
 package com.demoqa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PracticeFormTest {
+
+    @Test
+    void FirstTest(){
+        Assertions.assertTrue(true);
+
+    }
 
 }
